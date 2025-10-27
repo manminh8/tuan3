3871ef6ab7fec44e04cf1cfa2462252080aa131f
NguyenQuangThang <thang.nq421@gmail.com>
2025-10-27 10:35:15 +0700

    Ham tinh tich a, b

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,22 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+
 int main()
 {
+	int a, b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+	cout << "Tich la: "; 
+	tich(a, b)
 	system("pause");
 	return 0;
+}
+
+
+int tich(int a, int b) {
+	return a * b;
 }
