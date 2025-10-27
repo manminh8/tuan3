909c48866bf17e27afb435c52269fb3e8c94b315
Man <hominhman2004tn@gmail.com>
2025-10-27 10:36:16 +0700

    Ham tinh tong

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,13 @@ float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a = 5;
+	int b = 10;
+	cout << "Ham tong";
+	cout << tong(a,b);
 	system("pause");
 	return 0;
+}
+int tong(int a,int b){
+	return a+b;
 }
