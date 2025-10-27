47f653e48867b97e51f745223a659a0e454a66b6
VuongThaiTai <taivuong15062004@gmail.com>
2025-10-27 10:41:56 +0700

    Ham tinh hieu a - b

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,17 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a, b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+	cout << "Hieu a - b = " << hieu(a,b);
 	system("pause");
 	return 0;
+}
+
+int hieu(int a, int b){
+	return a - b;
 }
